25b3703d620f448c294cd76e2fc7e2f30ea05ab1
Abhijith Saji <117380427+Abhijith834@users.noreply.github.com>
2024-02-28 20:31:17 +0000

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const backgroundVideo = document.getElementById('backgroundVideo');
-
-window.addEventListener('scroll', function() {
-  const scrollPosition = window.pageYOffset;
-  const speed = 0.5; // Adjust this value to control the speed of parallax effect
-
-  backgroundVideo.style.top = scrollPosition * speed + 'px';
-});
